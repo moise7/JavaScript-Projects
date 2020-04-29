ccb5095f45df9b96d91c3a5bab868a3e2574531c
moise7 <63320773+moise7@users.noreply.github.com>
2020-04-28 22:12:45 -0400

    Add files via upload

--- Basic JavaScript Projects/Js/Basic_JavaScript_7.js
+++ Basic JavaScript Projects/Js/Basic_JavaScript_7.js
@@ -1,14 +1,16 @@
+var x = 10;
+function Add_number(){
+    console.log(30+x + "<br>")
+}
 function Time_function(){
     var Time = new Date().getHours();
     var Reply;
-    if (Time<12 == Time>0){
+    if (Time<24 == Time>0){
         Reply = "It is morning time!";
     }
     else{
         Reply = "it is evening time!"
     }
-    function Time_function(){
-        console.log("Time");
+
     document.getElementById("Time_of_day").innerHTML=Reply;
     }
-}
