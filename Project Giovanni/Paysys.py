a7074552eceb25224207972b0dafb685996df650
moise7 <watibonheur@gmail.com>
2020-08-04 16:58:14 -0400

    Project

---
+++ Project Giovanni/Paysys.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import ttk
+window = Tk()
+window.title("Payment system SMCAF ent.")
+window.geometry('400x400')
+window.configure(background = "red");
+a = Label(window ,text = "Full name").grid(row = 0,column = 0)
+b = Label(window ,text = "Bank's company ").grid(row = 1,column = 0)
+c = Label(window ,text = "Account number").grid(row = 2,column = 0)
+d = Label(window ,text = "Transfert amount ").grid(row = 3,column = 0)
+a1 = Entry(window).grid(row = 0,column = 1)
+b1 = Entry(window).grid(row = 1,column = 1)
+c1 = Entry(window).grid(row = 2,column = 1)
+d1 = Entry(window).grid(row = 3,column = 1)
+def clicked():
+   res = "Welcome to " + txt.get()
+   lbl.configure(text= res)
+btn = ttk.Button(window ,text="Submit").grid(row=4,column=0)
+window.mainloop()
