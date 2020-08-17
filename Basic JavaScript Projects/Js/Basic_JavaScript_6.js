1edcf8e409cb1af8ceddda98a0965f67d4714f2d
moise7 <63320773+moise7@users.noreply.github.com>
2020-08-17 19:15:09 -0400

    Add files via upload

--- Basic JavaScript Projects/Js/Basic_JavaScript_6.js
+++ Basic JavaScript Projects/Js/Basic_JavaScript_6.js
@@ -1,17 +1,17 @@
 function Ride_Function() {
     var Height, Can_ride;
-    Height = document.getElementById("Height").nodeValue;
+    Height = document.getElementById("Height").Value;
     Can_ride = (Height<52) ? "You are too short" : "You are tall engough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
-}
-function vehicle(make, model, year, color){
-    this.vehicle_make = make;
-    this.vehicle_model = model;
-    this.vehicle_year= year;
-    this.vehicle_color= color;
-}
-var Moise = new vehicle("Dodge","Viper", 2020, "red");
-var Emily = new vehicle("Jeep", "Trail", 2020,"White");
-function myFunction(){
-    document.getElementById("Keywords_and_Constructors").innerHTML = "Moise drives a "+ Moise.vehicle_color + "-colored" + Moise.vehicle_model + " manufactured in " + Moise.vehicle_year;
+    function Vehicle(make, model, year, color){
+        this.Vehicle_make = make;
+        this.Vehicle_model = model;
+        this.Vehicle_year= year;
+        this.Vehicle_color= color;
+    }
+    var Moise = new Vehicle("Dodge","Viper", 2020, "Red");
+    var Emily = new Vehicle("Jeep", "Trail", 2020,"White");
+    function myFunction(){
+        document.getElementById("Keywords_and_Constructors").innerHTML = "Moise drives a "+ Moise.vehicle_color + "-colored" + Moise.vehicle_model + " manufactured in " + Moise.vehicle_year;
+    }
 }
