c4e3dec1d365681c82047f64843ac729d4b5d34e
moise7 <63320773+moise7@users.noreply.github.com>
2020-08-19 23:20:06 -0400

    Add files via upload

--- Basic JavaScript Projects/Js/Basic_JavaScript_6.js
+++ Basic JavaScript Projects/Js/Basic_JavaScript_6.js
@@ -3,7 +3,8 @@ function Ride_Function() {
     Height = document.getElementById("Height").Value;
     Can_ride = (Height<52) ? "You are too short" : "You are tall engough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
-    function Vehicle(make, model, year, color){
+}
+function Vehicle(make, model, year, color){
         this.Vehicle_make = make;
         this.Vehicle_model = model;
         this.Vehicle_year= year;
@@ -11,7 +12,10 @@ function Ride_Function() {
     }
     var Moise = new Vehicle("Dodge","Viper", 2020, "Red");
     var Emily = new Vehicle("Jeep", "Trail", 2020,"White");
-    function myFunction(){
-        document.getElementById("Keywords_and_Constructors").innerHTML = "Moise drives a "+ Moise.vehicle_color + "-colored" + Moise.vehicle_model + " manufactured in " + Moise.vehicle_year;
+function myFunction(){
+       document.getElementById("Keywords_and_Constructors").innerHTML = "Moise drives a "+ Moise.vehicle_color + "-colored" + Moise.vehicle_model + " manufactured in " + Moise.vehicle_year;
+    function Nested_Function(){
+        var Starting_point = 8;
+        return Starting_point;
     }
-}
+    }
